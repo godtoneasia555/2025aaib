c0a74b11abd1ff1da632c6b31d0d9895159b349f
godtoneasia555 <jennie940914@gmail.com>
2025-03-12 12:01:11 +0800

    week04

---
+++ week04-1.c
@@ -0,0 +1,9 @@
+
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        ans=0
+        for i in range(1,n*2,2):
+            ans+=i
+        for i in range(1,n*2-1,2):
+            ans+=i
+        return ans
